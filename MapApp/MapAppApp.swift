67c3dfe57be4e1fe2f93ea695fb2a9516fc4e884
Rumen Guin <rumen2k@gmail.com>
2022-01-28 11:44:56 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by RUMEN GUIN on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
